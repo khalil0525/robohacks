ef1553e1bad75777809374f0207896bd80e8342e
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-01 17:48:10 -0400

    added path

--- backend/services/AudioDownloaderService.js
+++ backend/services/AudioDownloaderService.js
@@ -2,24 +2,31 @@ const { MediaDownloader } = require("../services/media-downloader");
 const ytdl = require("ytdl-core");
 const ffmpeg = require("fluent-ffmpeg");
 const { MediaConverter } = require("../services/media-converter");
+const path = require("path"); // Import the path module
+
 const audioPreset = {
-  quality: "highestaudio",
-  filter: "audioonly",
+	quality: "highestaudio",
+	filter: "audioonly",
 };
 
 class YtAudioDownloadService {
-  async downloadAudio(videoUrl) {
-    const youtubeDownloader = new MediaDownloader(ytdl);
-    youtubeDownloader.setStream(videoUrl, audioPreset);
-    const videoId = await youtubeDownloader.getVideoId(videoUrl);
-    console.log(videoId);
-    const audioConverter = new MediaConverter(ffmpeg);
-    const stream = youtubeDownloader.getStream();
-    const fileDestination = `./audio-files/${videoId}.wav`;
-    await audioConverter.convertAudio(stream, "wav", fileDestination);
+	async downloadAudio(videoUrl) {
+		const youtubeDownloader = new MediaDownloader(ytdl);
+		youtubeDownloader.setStream(videoUrl, audioPreset);
+		const videoId = await youtubeDownloader.getVideoId(videoUrl);
+		console.log(videoId);
+		const audioConverter = new MediaConverter(ffmpeg);
+		const stream = youtubeDownloader.getStream();
+		const fileDestination = path.join(
+			__dirname,
+			"audio-files",
+			`${videoId}.wav`
+		);
 
-    return fileDestination;
-  }
+		await audioConverter.convertAudio(stream, "wav", fileDestination);
+
+		return fileDestination;
+	}
 }
 
 exports.YtAudioDownloadService = YtAudioDownloadService;
