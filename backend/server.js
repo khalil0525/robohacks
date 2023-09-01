c53b3b1bb71b099e7101a6bcc7af145c7d5d15f4
Khalil Collins <Collinskhalil@hotmail.com>
2023-08-31 23:09:00 -0400

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,27 @@ const app = express();
 const port = 5000;
 const cors = require("cors");
 const { socket } = require("./utils/socket");
+const path = require("path");
+
 const server = require("http").createServer(app);
 socket.init(server);
 
 const {
-  youtubeTranscriberController,
+	youtubeTranscriberController,
 } = require("./controller/youtube-transcriber");
 
 const { youtubeTestController } = require("./controller/youtube-transcriber");
 require("dotenv").config();
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(cors());
 app.use(express.json());
 
 app.post("/api/transcribe", youtubeTranscriberController);
 app.post("/test/transcribe", youtubeTestController);
+app.use(express.static(path.join(__dirname, "build")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
 server.listen(port);
