54b62d0b09ec1eceed835a4926381536e2f26b8b
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-01 17:36:24 -0400

    added in env vars

--- backend/services/text-summary.js
+++ backend/services/text-summary.js
@@ -1,18 +1,19 @@
 class TextSummary {
-  constructor(model) {
-    this.model = model;
-    model.init(process.env.COHERE_API);
-  }
-  async generate(text) {
-    const generateResponse = await this.model.generate({
-      model: "medium",
-      prompt: text,
-      max_tokens: 50,
-      temperature: 1,
-    });
+	constructor(model) {
+		this.model = model;
+		console.log(process.env.COHERE_API_KEY);
+		model.init(process.env.COHERE_API_KEY);
+	}
+	async generate(text) {
+		const generateResponse = await this.model.generate({
+			model: "medium",
+			prompt: text,
+			max_tokens: 50,
+			temperature: 1,
+		});
 
-    return generateResponse.body.generations[0].text;
-  }
+		return generateResponse.body.generations[0].text;
+	}
 }
 
 exports.TextSummary = TextSummary;
